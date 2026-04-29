c1f11f5a6bb9e141e4c5c58e33b00e50ee583841
5av9385555 <5av9385555@gmail.com>
2026-04-29 17:01:26 +0300

    Update test_api.py

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,7 +1,51 @@
 from app.api import app
 
+
 def test_health():
     client = app.test_client()
     response = client.get("/health")
+
     assert response.status_code == 200
     assert response.json["status"] == "healthy"
+
+
+def test_predict():
+    client = app.test_client()
+
+    data = {
+        "LIMIT_BAL": 20000,
+        "SEX": 2,
+        "EDUCATION": 2,
+        "MARRIAGE": 1,
+        "AGE": 24,
+        "PAY_0": 2,
+        "PAY_2": 2,
+        "PAY_3": -1,
+        "PAY_4": -1,
+        "PAY_5": -2,
+        "PAY_6": -2,
+        "BILL_AMT1": 3913,
+        "BILL_AMT2": 3102,
+        "BILL_AMT3": 689,
+        "BILL_AMT4": 0,
+        "BILL_AMT5": 0,
+        "BILL_AMT6": 0,
+        "PAY_AMT1": 0,
+        "PAY_AMT2": 689,
+        "PAY_AMT3": 0,
+        "PAY_AMT4": 0,
+        "PAY_AMT5": 0,
+        "PAY_AMT6": 0,
+        "avg_delay": -0.3333,
+        "max_delay": 2,
+        "avg_bill": 1284,
+        "avg_pay": 114.83,
+        "utilization": 0.19565
+    }
+
+    response = client.post("/predict", json=data)
+
+    assert response.status_code == 200
+    assert "prediction" in response.json
+    assert "probability" in response.json
+    assert "model_version" in response.json
