5ad99f121b8db6d5b3ef0e5a778475341bf923bb
5av9385555 <5av9385555@gmail.com>
2026-04-29 14:32:42 +0300

    Update model_handler.py

--- app/model_handler.py
+++ app/model_handler.py
@@ -1,1 +1,21 @@
+import joblib
+import pandas as pd
 
+MODEL_PATH = "models/model_v1.pkl"
+FEATURES_PATH = "models/features_v1.pkl"
+
+model = joblib.load(MODEL_PATH)
+features = joblib.load(FEATURES_PATH)
+
+def predict_default(data: dict):
+    X = pd.DataFrame([data])
+    X = X[features]
+
+    prediction = model.predict(X)[0]
+    probability = model.predict_proba(X)[0][1]
+
+    return {
+        "prediction": int(prediction),
+        "probability": float(probability),
+        "model_version": "v1"
+    }
